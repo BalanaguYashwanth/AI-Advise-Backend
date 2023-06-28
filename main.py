e59b66b4a0b7882f7c75d791896cd46eaa72ade4
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2023-06-29 00:08:12 +0530

    updated bard api config

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import os
 import json
 import requests
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI
 from pydantic import BaseModel
 from Bard import Chatbot
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from starlette.responses import StreamingResponse
-
+import google.generativeai as palm
 
 app = FastAPI()
 handler = Mangum(app)
+palm.configure(api_key=os.environ['BARD_API_KEY'])
 
 origins = [
     ###### we should allow only frontend url ########
     "http://localhost:3000",
-    "https://multi-chat-ai.web.app"
+    "https://multi-chat-ai.web.app",
+     "https://knowr.co",
 ]
 
 app.add_middleware(
@@ -31,7 +33,7 @@ class TextInput(BaseModel):
     message:str
 
 @app.post('/v1/chat/completions')
-async def completions(message: TextInput):
+async def completions(contents: TextInput):
 
     open_ai_key =  os.environ['OPENAI_API_KEY']
     open_ai_url =  os.environ['OPENAI_API_URL']
@@ -47,7 +49,7 @@ async def completions(message: TextInput):
       "messages": [
         {
           "role": "user",
-          "content": message.message,
+          "content": contents.message,
         },
       ],
       # "max_tokens": 100, #due to max tokens asnwers are limited response
@@ -64,22 +66,48 @@ async def completions(message: TextInput):
 async def status():
     return {'status':'healthy'}
 
+# @app.post("/ask")
+# async def ask(request: Request, message: TextInput):
+#     # Get the user-defined auth key from the environment variables
+#     user_auth_key = os.environ.get('USER_AUTH_KEY')
+#     session_id = os.environ['SESSION_ID']
+
+#     # Check if the user has defined an auth key,
+#     # If so, check if the auth key in the header matches it.
+#     if user_auth_key and user_auth_key != request.headers.get('Authorization'):
+#         raise HTTPException(status_code=401, detail='Invalid authorization key')
+
+#     # Execute your code without authenticating the resource
+#     try:
+#       chatbot = Chatbot(session_id)
+#       response = chatbot.ask(message.message)
+#       return response
+#     except Exception as e:
+#       print("An exception occurred",e)
+
+# @app.post("/ask")
+# async def ask(contents: TextInput):
+#     # Get the user-defined auth key from the environment variables
+#     bard_ai_key =  os.environ['BARD_API_KEY']
+#     bard_api_url = os.environ['BARD_API_URL']
+
+#     requestPayload = {
+#       "prompt": {
+#           "text": contents.message
+#       }
+#     }
+
+#     requestPayload = json.dumps(requestPayload)
+#     try:
+#       response = requests.post(f'{bard_api_url}?key={bard_ai_key}',requestPayload)
+#       return JSONResponse(response.json())
+#     except Exception as e:
+#       print("An exception occurred",e)
+
 @app.post("/ask")
-async def ask(request: Request, message: TextInput):
-    # Get the user-defined auth key from the environment variables
-    user_auth_key = os.environ.get('USER_AUTH_KEY')
-    session_id = os.environ['SESSION_ID']
-
-    # Check if the user has defined an auth key,
-    # If so, check if the auth key in the header matches it.
-    if user_auth_key and user_auth_key != request.headers.get('Authorization'):
-        raise HTTPException(status_code=401, detail='Invalid authorization key')
-
-    # Execute your code without authenticating the resource
+async def ask(contents: TextInput):
     try:
-      chatbot = Chatbot(session_id)
-      response = chatbot.ask(message.message)
-      return response
+       response = palm.chat(messages=contents.message)
+       return response.last
     except Exception as e:
       print("An exception occurred",e)
-
--- start-app.sh
+++ start-app.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-uvicorn main:app --reload --port 8000
+
+cd "$(dirname "$0")"
+source venv/bin/activate
+uvicorn main:app --reload --port 8000 --host 0.0.0.0 --workers 4
+
+# echo "$(pwd)"
+# uvicorn main:app --reload --port 8000
